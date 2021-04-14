4f47efcd40a6746408f0a73ccfe8044fe3960d91
SIDDARDH REDDY GARLAPATI <siddarthreddyg4@gmail.com>
2021-04-14 16:17:10 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$conn = mysqli_connect('localhost','id16590793_root','1Fa+jW6pR!2N/(4K','id16590793_grip_bank');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
